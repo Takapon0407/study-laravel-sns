bd9b979874d5dc9e51fd0d9440722acbceb3f20c
Takapon0407 <takapon_business@yahoo.co.jp>
2020-11-12 23:01:00 +0900

    bladeに言い値をした記事一覧を渡すようにアクション追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,4 +47,16 @@ class UserController extends Controller
 
         return ['name' => $name];
     }
+
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
 }
